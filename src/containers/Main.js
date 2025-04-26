19e214742c6031aebf00b7333393b4fd8d71d5d1
unknown <eyescream16wind@proton.me>
2025-04-26 14:10:20 -0300

    commit inicial

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,9 +4,6 @@ import Home from "../pages/home/HomeComponent";
 import Splash from "../pages/splash/Splash";
 import Education from "../pages/education/EducationComponent";
 import Experience from "../pages/experience/Experience";
-import Opensource from "../pages/opensource/Opensource";
-import Contact from "../pages/contact/ContactComponent";
-import Projects from "../pages/projects/Projects";
 import { settings } from "../portfolio.js";
 import Error404 from "../pages/errors/error404/Error";
 
@@ -43,16 +40,6 @@ export default class Main extends Component {
               <Education {...props} theme={this.props.theme} />
             )}
           />
-          <Route
-            path="/opensource"
-            render={(props) => (
-              <Opensource {...props} theme={this.props.theme} />
-            )}
-          />
-          <Route
-            path="/contact"
-            render={(props) => <Contact {...props} theme={this.props.theme} />}
-          />
 
           {settings.isSplash && (
             <Route
@@ -60,11 +47,6 @@ export default class Main extends Component {
               render={(props) => <Splash {...props} theme={this.props.theme} />}
             />
           )}
-
-          <Route
-            path="/projects"
-            render={(props) => <Projects {...props} theme={this.props.theme} />}
-          />
           <Route
             path="*"
             render={(props) => <Error404 {...props} theme={this.props.theme} />}
